e157e6d25d3c04d9347f422cf2dc8a7e636d7419
vikle <ialucard@yandex.ru>
2025-06-13 15:11:23 +0300

    add actor bakers logic

--- Runtime/Unity/EntityActor.cs
+++ Runtime/Unity/EntityActor.cs
@@ -12,22 +12,31 @@ namespace UniversalEntities
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
 #endif
     [DisallowMultipleComponent]
-    public abstract class EntityActor : MonoBehaviour
+    public sealed class EntityActor : MonoBehaviour
     {
+        public enum EDataComponent : byte
+        {
+            SingleEntityActorData,
+            SeparatedObjectRef,
+            Excluded
+        };
+        
+        public EDataComponent dataComponent;
+        
         public Entity EntityRef
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]get; 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]private set;
         }
         
-        EntityActorComponent[] m_attachedComponents;
+        EntityActorBaker[] m_backers;
 
         Pipeline m_pipeline;
 
         void Awake()
         {
             m_pipeline = UniversalEntitiesEngine.PipelineInstance;
-            InitAttachedComponents();
+            InitBakers();
         }
 
         void OnEnable()
@@ -44,19 +53,32 @@ namespace UniversalEntities
         {
             if (EntityRef != null) return;
             
-            InitAttachedComponents();
+            InitBakers();
             
             var entity = m_pipeline.CreateEntity();
 
-            entity.AddComponent<ObjectRef<GameObject>>().Target = gameObject;
-            entity.AddComponent<ObjectRef<Transform>>().Target = transform;
-            entity.AddComponent<ObjectRef<EntityActor>>().Target = this;
-            
-            for (int i = 0, i_max = m_attachedComponents.Length; i < i_max; i++)
+            switch (dataComponent)
             {
-                var instance = m_attachedComponents[i];
-                entity.AddComponent(instance);
+                case EDataComponent.SingleEntityActorData:
+                    var data = entity.AddComponent<EntityActorData>();
+                    data.gameObject = gameObject;
+                    data.transform = transform;
+                    data.actor = this;
+                    break;
+                case EDataComponent.SeparatedObjectRef: 
+                    entity.AddComponent<ObjectRef<GameObject>>().Target = gameObject;
+                    entity.AddComponent<ObjectRef<Transform>>().Target = transform;
+                    entity.AddComponent<ObjectRef<EntityActor>>().Target = this;
+                    break;
+                default: break;
             }
+            
+            for (int i = 0, i_max = m_backers.Length; i < i_max; i++)
+            {
+                m_backers[i].OnAfterEntityCreated(m_pipeline, EntityRef, this);
+            }
+            
+            entity.Initialize();
 
             EntityRef = entity;
         }
@@ -66,23 +88,22 @@ namespace UniversalEntities
             var entity = EntityRef;
             
             if (entity == null) return;
-
-            m_pipeline.DestroyEntity(entity);
             
-            for (int i = 0, i_max = m_attachedComponents.Length; i < i_max; i++)
+            for (int i = 0, i_max = m_backers.Length; i < i_max; i++)
             {
-                var instance = m_attachedComponents[i];
-                // entity.RemoveComponent(instance);
+                m_backers[i].OnBeforeEntityDestroyed(m_pipeline, EntityRef, this);
             }
+            
+            m_pipeline.DestroyEntity(entity);
             
             EntityRef = null;
         }
 
-        private void InitAttachedComponents()
+        private void InitBakers()
         {
-            if (m_attachedComponents == null)
+            if (m_backers == null)
             {
-                m_attachedComponents = GetComponents<EntityActorComponent>();
+                m_backers = GetComponents<EntityActorBaker>();
             }
         }
     };
---
+++ Runtime/Unity/EntityActorBaker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+#if ENABLE_IL2CPP
+using Unity.IL2CPP.CompilerServices;
+#endif
+
+namespace UniversalEntities
+{
+#if ENABLE_IL2CPP
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+#endif
+    [RequireComponent(typeof(EntityActor))]
+    public abstract class EntityActorBaker : MonoBehaviour
+    {
+        public abstract void OnAfterEntityCreated(Pipeline pipeline, Entity entity, EntityActor actor);
+        
+        public abstract void OnBeforeEntityDestroyed(Pipeline pipeline, Entity entity, EntityActor actor);
+    };
+}
